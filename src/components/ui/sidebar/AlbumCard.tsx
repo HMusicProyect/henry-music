0fd9b938899c3fe6f38153f0bba5284a9ab1d67c
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-04-11 18:29:55 -0300

    card estilos

--- src/components/ui/sidebar/AlbumCard.tsx
+++ src/components/ui/sidebar/AlbumCard.tsx
@@ -3,7 +3,7 @@ import Card from './Card';
 const AlbumCard = () => {
     //aca podrian llegar por props los datos de cada album 
     return (
-        <Card className="w-64">
+        <Card className="w-64 bg-stone-900 transition duration-400 ease-in-out hover:bg-neutral-800">
             <img src="https://ik.imagekit.io/loudcave/wp-content/uploads/2021/05/4ab07de8032ab00b91b04061a687ad00.1000x1000x1.png" alt="Album cover" className="w-full h-48 object-cover rounded-t-lg" />
             <div className="p-4">
                 <h2 className="text-lg font-semibold text-white">Nombre del álbum</h2>
