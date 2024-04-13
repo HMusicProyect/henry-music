c592f75113ca71ded577ba7db9d065916a02eae8
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-04-12 21:11:12 -0300

    buscador ampliado ej

--- src/components/ui/sidebar/data.ts
+++ src/components/ui/sidebar/data.ts
@@ -2,28 +2,32 @@
 export const data = [
     {
         imageUrl: "https://akamai.sscdn.co/uploadfile/letras/albuns/6/c/2/7/1068271619182076.jpg",
-        songName: "duki",
-        artistName: "Artista 1"
+        songName: "Malvec",
+        artistName: "Duki"
       },
       {
         imageUrl: "https://f4.bcbits.com/img/a0465333642_10.jpg",
-        songName: "Album 2",
-        artistName: "Artista 2"
+        songName: "Stones Album",
+        artistName: "Roling Stones"
       },
       {
         imageUrl: "https://radiotubers.los40.com/los40/imagenes/2017/11/28/album/1511885438_220718_1512040978_album_normal.jpg",
-        songName: "Album 3",
+        songName: "Imagine Dragons",
         artistName: "Artista 3"
       },
       {
         imageUrl: "https://www.cinconoticias.com/wp-content/uploads/tipos-de-albumes.jpg",
-        songName: "Album 3",
-        artistName: "Artista 3"
+        songName: "Album Gnses",
+        artistName: "Guns Roses"
       },
       {
         imageUrl: " https://lastfm.freetls.fastly.net/i/u/ar0/e220bd192e63a48e6fddda59f3fc7662.jpg",
-        songName: "Album 3",
-        artistName: "Artista 3"
+        songName: "Bad Bunny",
+        artistName: "Bad Bunny"
+      } , {
+        imageUrl: " https://i.scdn.co/image/ab67616d0000b27315583045b2fdb7d7bab10e81",
+        songName: "BZR sesion",
+        artistName: "Bizarrap"
       }
   ];
   
