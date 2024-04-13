69d10b5a59683f5f5f6570ee86c267edb146f405
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-04-13 13:09:57 -0300

    filtro por año lanzamiento

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -6,6 +6,7 @@ import AlbumCard from "@/components/ui/sidebar/AlbumCard";
 const Search: React.FC = () => {
   const [filteredData, setFilteredData] = useState(data);
   const [query, setQuery] = useState('');
+  const [orderBy, setOrderBy] = useState('desc');
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = event.target.value;
@@ -18,23 +19,43 @@ const Search: React.FC = () => {
     setFilteredData(filtered);
   };
 
+  const handleSort = (selectedOrder: string) => {
+    const sortedData = [...filteredData];
+    sortedData.sort((a, b) => {
+      if (selectedOrder === 'asc') {
+        return a.releaseYear - b.releaseYear;
+      } else {
+        return b.releaseYear - a.releaseYear;
+      }
+    });
+    setFilteredData(sortedData);
+    setOrderBy(selectedOrder);
+  };
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-      <div className="mt-2 mb-7 px-6">
-        <input
-          type="text"
-          value={query}
-          onChange={handleSearch}
-          placeholder="Buscar canción o artista..."
-          className="px-4 py-2 border rounded-md bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-          {filteredData.map((item, index) => (
-            <AlbumCard key={index} {...item} />
-          ))}
-        </div>
-      </div>
+    <div className="mt-2 mb-7 px-6 flex justify-between items-center">
+      <input
+        type="text"
+        value={query}
+        onChange={handleSearch}
+        placeholder="Buscar canción o artista..."
+        className="px-4 py-2 border rounded-full bg-gray-800 text-white placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 w-72 "
+      />
+      <select
+        value={orderBy}
+        onChange={(e) => handleSort(e.target.value)}
+        className="mt-2 px-4 py-2 border rounded-full bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        <option value="desc">Fecha de estreno (descendente)</option>
+        <option value="asc">Fecha de estreno (ascendente)</option>
+      </select>
     </div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+      {filteredData.map((item, index) => (
+        <AlbumCard key={index} {...item} />
+      ))}
+    </div>
+  </div>
   );
 };
 
--- src/components/ui/sidebar/data.ts
+++ src/components/ui/sidebar/data.ts
@@ -1,33 +1,40 @@
 // data.ts
 export const data = [
-    {
-        imageUrl: "https://akamai.sscdn.co/uploadfile/letras/albuns/6/c/2/7/1068271619182076.jpg",
-        songName: "Malvec",
-        artistName: "Duki"
-      },
-      {
-        imageUrl: "https://f4.bcbits.com/img/a0465333642_10.jpg",
-        songName: "Stones Album",
-        artistName: "Roling Stones"
-      },
-      {
-        imageUrl: "https://radiotubers.los40.com/los40/imagenes/2017/11/28/album/1511885438_220718_1512040978_album_normal.jpg",
-        songName: "Imagine Dragons",
-        artistName: "Artista 3"
-      },
-      {
-        imageUrl: "https://www.cinconoticias.com/wp-content/uploads/tipos-de-albumes.jpg",
-        songName: "Album Gnses",
-        artistName: "Guns Roses"
-      },
-      {
-        imageUrl: " https://lastfm.freetls.fastly.net/i/u/ar0/e220bd192e63a48e6fddda59f3fc7662.jpg",
-        songName: "Bad Bunny",
-        artistName: "Bad Bunny"
-      } , {
-        imageUrl: " https://i.scdn.co/image/ab67616d0000b27315583045b2fdb7d7bab10e81",
-        songName: "BZR sesion",
-        artistName: "Bizarrap"
-      }
-  ];
+  {
+    imageUrl: "https://akamai.sscdn.co/uploadfile/letras/albuns/6/c/2/7/1068271619182076.jpg",
+    songName: "Malvec",
+    artistName: "Duki",
+    releaseYear: 2021
+  },
+  {
+    imageUrl: "https://f4.bcbits.com/img/a0465333642_10.jpg",
+    songName: "Stones Album",
+    artistName: "Rolling Stones",
+    releaseYear: 1971
+  },
+  {
+    imageUrl: "https://radiotubers.los40.com/los40/imagenes/2017/11/28/album/1511885438_220718_1512040978_album_normal.jpg",
+    songName: "Imagine Dragons",
+    artistName: "Artista 3",
+    releaseYear: 2017
+  },
+  {
+    imageUrl: "https://www.cinconoticias.com/wp-content/uploads/tipos-de-albumes.jpg",
+    songName: "Album Gnses",
+    artistName: "Guns N' Roses",
+    releaseYear: 1987
+  },
+  {
+    imageUrl: "https://lastfm.freetls.fastly.net/i/u/ar0/e220bd192e63a48e6fddda59f3fc7662.jpg",
+    songName: "Bad Bunny",
+    artistName: "Bad Bunny",
+    releaseYear: 2022
+  },
+  {
+    imageUrl: "https://i.scdn.co/image/ab67616d0000b27315583045b2fdb7d7bab10e81",
+    songName: "BZR sesion",
+    artistName: "Bizarrap",
+    releaseYear: 2020
+  }
+];
   
