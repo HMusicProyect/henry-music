e7b1686a26497a7b3e884a4a3a1bc99d5d41af00
alan damian Fernandez <fernandeezalan20@gmail.com>
2024-04-17 09:13:38 -0300

    register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,68 +1,65 @@
-"use client"
-import React, { useState } from 'react';
-import { useRouter } from "next/navigation";
-import { useStore } from '@/store/user.store';
+import { Metadata } from "next";
 
-type User = {
-    name: string;
-    email: string;
-    password: string;
+import Link from "next/link";
+import { Command } from "lucide-react";
+import { UserRegisterForm } from "@/components/user-register-auth";
+import AuthButton from "@/components/auth-button";
+
+export const metadata: Metadata = {
+  title: "Autenticação",
+  description:
+    "Autenticação utilizando Shadcs-ui Next-Auth (Authjs), prisma e Nextjs 13.4+",
 };
 
-const RegisterPage: React.FC = () => {
-    const router = useRouter();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const postUser = useStore((state) => state.postUser);
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-
-        const newUser: User = {
-            name,
-            email,
-            password,
-        };
-
-        try {
-        const response = await postUser(newUser);
-        
-        if (response && response.ok) { 
-            router.push('/login'); 
-        }
-        } catch (error) {
-            alert('Ocurrió un error al registrar el usuario.');
-        }
-    };
-
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-2">
-            <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
-                <div className="flex justify-center items-center">
-                <span className="text-gray-700 font-semibold text-2xl">Register Page</span>
-                </div>
-                <form className="mt-4" onSubmit={handleSubmit}>
-                <label className="block">
-                    <span className="text-gray-700 text-sm">Name:</span>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <label className="block mt-3">
-                    <span className="text-gray-700 text-sm">Email:</span>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <label className="block mt-3">
-                    <span className="text-gray-700 text-sm">Password:</span>
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <div className="mt-6">
-                    <button type="submit" className="py-2 px-4 text-center bg-indigo-600 rounded-md w-full text-white text-sm hover:bg-indigo-500">Register</button>
-                </div>
-                </form>
-            </div>
+export default async function Register() {
+  return (
+    <div className="container relative py-48 md:py-0  h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <AuthButton page="register" />
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="absolute inset-0 bg-zinc-900" />
+        <div className="relative z-20 flex items-center text-lg font-medium">
+          <Command className="mr-2 h-6 w-6" /> DeveloperDeck101
         </div>
-    );
-};
-
-export default RegisterPage;
+        <div className="relative z-20 mt-auto">
+          <blockquote className="space-y-2">
+            <p className="text-lg">
+              &ldquo;Esta apicação é um guia para utilização do Next-Auth
+              (Authjs) em conjunto com Prisma (MongoDB) e NextJs 13.4+.&rdquo;
+            </p>
+            <footer className="text-sm">Bruno Kilian</footer>
+          </blockquote>
+        </div>
+      </div>
+      <div className="lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Criar Conta
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Entre com seus dados
+            </p>
+          </div>
+          <UserRegisterForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Termos de Serviço
+            </Link>{" "}
+            e{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Política de Privacidade
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
