c877332af9ec999138ad2659ea2477ca21e899b3
alan damian Fernandez <fernandeezalan20@gmail.com>
2024-04-22 11:55:58 -0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,14 @@ const nextConfig = {
   images: {
     domains: [
       '1.bp.blogspot.com', 
-    'localhost:3001', 
-    'i1.sndcdn.com', 
-    'hmusic.blob.core.windows.net', 
-    'st3.depositphotos.com',
-    'www.clipartkey.com',
-    'www.accessselfstorage.com', 
-    'worldwildfilms.com'],
+      'localhost:3001', 
+      'i1.sndcdn.com', 
+      'hmusic.blob.core.windows.net', 
+      'st3.depositphotos.com',
+      'www.clipartkey.com',
+      'www.accessselfstorage.com', 
+      'worldwildfilms.com'
+    ],
   },
 };
 
