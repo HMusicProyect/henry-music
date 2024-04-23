951381693d014a90ed993781c651800e620b3d8d
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-04-23 17:26:59 -0300

    heander href="#"

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -126,9 +126,9 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                                     <Menu.Item>
                                     {({ active }) => (
                                         <Link
-                                        href="/any"
-                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                        onClick={handlePremium}
+                                            href="#"
+                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                            onClick={handlePremium}
                                         >
                                         Premium
                                         </Link>
