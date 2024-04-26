d98e9a2afdec89463914619b91433ad757bbab16
alan damian Fernandez <92124271+Alan-Fernandez@users.noreply.github.com>
2024-04-26 14:35:48 -0300

    config Domains  image

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,13 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    domains: [
-      '1.bp.blogspot.com', 
-      'localhost:3001', 
-      'i1.sndcdn.com', 
-      'hmusic.blob.core.windows.net', 
-      'st3.depositphotos.com',
-      'www.accessselfstorage.com', 
-      'worldwildfilms.com',
-      'lh3.googleusercontent.com',
-      'hmusic-server-production.up.railway.app',
-      'res.cloudinary.com',
-      'www.clipartkey.com',
-      'https://hmusic-server.up.railway.app/',
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
