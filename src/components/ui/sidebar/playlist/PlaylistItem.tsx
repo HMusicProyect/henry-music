32dba5f552a30f1abda1cd572ac86b573514760a
alan damian Fernandez <fernandeezalan20@gmail.com>
2024-05-05 14:02:17 -0300

    refacturacion

--- src/components/ui/sidebar/playlist/PlaylistItem.tsx
+++ src/components/ui/sidebar/playlist/PlaylistItem.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { capitalizeWords } from "@/utils/CapitalizeWords";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,44 +15,34 @@ interface MediaItemProps {
     onClick?: (id: string) => void;
 }
 
-
-const PlaylistItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+const PlaylistItem: React.FC<MediaItemProps> = ({ data }) => {
     if (!data) {
         return null;
     }
+
     const nameUrl = data.name.replace(/\s/g, '-');
     const capitalizedPlaylistName = capitalizeWords(data.name);
-    const currentSong = data.image ? data : { image: '/images/Henrry music.svg'};
-
-    // const handleClick = () => {
-    //     if (onClick) {
-    //         return onClick(data.id)
-    //     }
-    // }
+    const imageUrl = data.image || '/images/HenrryMusic.svg';
 
     return (
-        <Link
-            href={`/home/playlist/${nameUrl}?id=${data?.id}`}
-        >
-            <div
-                className="flex items-center gap-x-3 hover:bg-neutral-400/5 bg-neutral-800/5 cursor-pointer transition w-full p-2 rounded-md"
-            >
-                    <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
-                        <Image 
-                            layout='fill' 
-                            src={data.image ? data.image : '/images/HenrryMusic.svg'}
-                            alt="Media Item" 
-                            className="object-cover" 
-                        />
-                    </div>
-                    <div className="flex flex-col gap-y-1 overflow-hidden">
-                        <p className="text-white truncate">
-                            {capitalizedPlaylistName}
-                        </p>
-                    </div>
+        <Link href={`/home/playlist/${nameUrl}?id=${data.id}`}>
+            <div className="flex items-center gap-x-3 hover:bg-neutral-400/5 bg-neutral-800/5 cursor-pointer transition w-full p-2 rounded-md">
+                <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+                    <Image 
+                        layout='fill' 
+                        src={imageUrl}
+                        alt="Media Item" 
+                        className="object-cover" 
+                    />
+                </div>
+                <div className="flex flex-col gap-y-1 overflow-hidden">
+                    <p className="text-white truncate">
+                        {capitalizedPlaylistName}
+                    </p>
+                </div>
             </div>
         </Link>
     )
 }
 
-export default PlaylistItem
+export default PlaylistItem;
