4de151c609e8b38dedd9824f5cc3da7f36ff0600
juanmanuel <juanmanuelramoslencina@gmail.com>
2024-05-14 13:21:28 -0300

    estilos foter

--- src/components/ui/foter/foter.tsx
+++ src/components/ui/foter/foter.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import "./foter.css"
 
 interface FooterProps {
   companyName: string;
@@ -10,7 +11,7 @@ const Footer: React.FC<FooterProps> = ({ companyName, year }) => {
   return (
 
 
-	<footer className="bg-black rounded-lg shadow dark:bg-gray-900 m-4">
+	<footer className="bg-black rounded-lg shadow dark:bg-gray-900 m-4 foter">
 	<div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 	  <div className="sm:flex sm:items-center sm:justify-between">
 		<a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
